2299f296e23d5c205a90d36fa67fe24b15edcd1a
Mahfuz RP <mahfuzrpsmorg@gmail.com>
2023-02-28 19:56:49 +0600

    Defference of var let and const in js

---
+++ var-let-const.js
@@ -0,0 +1,18 @@
+// Example of var
+var x = 10;
+console.log(x);
+var x = 20;
+console.log(x);
+
+// two time declaretion is error in let {that's why we use it}
+let y = 'let declare';
+console.log(y);
+// let y = 'againt let declare';
+y = 'change the value of let'
+console.log(y);
+
+// Example of const
+const z = 190001;
+console.log(z);
+// z = 55555; // const don't allowed to change their value
+// console.log(z);
